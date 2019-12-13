f8990646ffd9039863d41241612ff5434cc297d8
Ville-Matti Hakanpää <ville-matti.hakanpaa@futuriot.com>
2019-12-13 13:08:57 +0200

    Add server functionality

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,29 @@
 import 'package:rnd_num/rnd_num.dart';
+import 'dart:io';
+import 'dart:convert';
 
-main(List<String> arguments) {
-  int num = 16;
-  if (arguments.length > 0) {
-    num = int.parse(arguments[0]);
+main(List<String> arguments) async {
+  var server = await HttpServer.bind(InternetAddress.loopbackIPv4, 3000);
+  print('Listening localhos:${server.port}');
+
+  await for (HttpRequest request in server) {
+    if (request.method == 'POST') {
+      if (request.uri.queryParameters['length'] != null) {
+        var num = int.parse(request.uri.queryParameters['length']);
+        var rndNum = randomBytes(num);
+
+        request.response
+          ..write(rndNum)
+          ..close();
+      } else {
+        request.response
+          ..write("Incorrect params! Length must be assigned!")
+          ..close();
+      }
+    } else {
+      request.response
+        ..write('Unsupportted method! Please use post!')
+        ..close();
+    }
   }
-  print(randomBytes(num));
 }
